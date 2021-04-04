d338a7cbc967304a6621024079c84da7478c0089
jasonyuchen <jasonyuchen@foxmail.com>
2021-04-05 01:11:30 +0800

    play with seastar

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
 #include <seastar/core/app-template.hh>
 #include <seastar/core/reactor.hh>
+#include <seastar/core/coroutine.hh>
+#include <seastar/core/sleep.hh>
+#include <seastar/core/when_all.hh>
 #include <iostream>
+#include <chrono>
+
+using namespace std;
+using namespace chrono_literals;
+
+seastar::future<> getstatus()
+{
+  std::cout << 1 << std::endl;
+  co_await seastar::sleep(1s);
+  std::cout << 2 << std::endl;
+  std::cout << "Hello world from" << std::endl;
+  auto a = co_await seastar::file_stat("/home/ubuntu");
+  std::cout << a.size << std::endl;
+  co_await seastar::sleep(1s);
+  std::cout << "seastar" << std::endl;
+  co_return;
+}
 
 int main(int argc, char** argv) {
   seastar::app_template app;
   app.run(argc, argv, [] {
-    std::cout << "Hello world from seastar" << std::endl;
-    return seastar::make_ready_future<>();
+    int *a = new int[1];
+    return getstatus();
   });
 }
