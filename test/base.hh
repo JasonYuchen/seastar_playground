555d340a43ec42acc614c1210c61ff366d7a3a64
jasonyuchen <jasonyuchen@foxmail.com>
2021-09-19 12:28:57 +0800

    try gtest

--- test/base.hh
+++ test/base.hh
@@ -4,7 +4,38 @@
 
 #pragma once
 
-#include <seastar/testing/test_case.hh>
+#include <thread>
 
-#define RAFTER_TEST_CASE(name) \
-  SEASTAR_TEST_CASE(__file__ ## name)
+#include <gtest/gtest.h>
+#include <seastar/core/app-template.hh>
+#include <seastar/core/future.hh>
+#include <seastar/core/reactor.hh>
+#include <seastar/core/coroutine.hh>
+
+
+#define RAFTER_TEST_CASE_F(suite, name) \
+  TEST_F(suite, name) {                 \
+    auto f =                             \
+  }                                     \
+  seastar::future<> GTEST_TEST_CLASS_NAME_(suite, name)::SeastarTestBody()
+
+class rafter_test_base {
+ public:
+  inline static const int argc = 3;
+  inline static const char* argv[] = {"rafter_test_base", ""};
+  rafter_test_base() {
+    engine_thread_ = std::thread([this] {
+      return app_.run(argc, const_cast<char **>(argv), []() -> seastar::future<> {
+        // TODO: handle signal
+        co_return;
+      });
+    });
+  }
+  ~rafter_test_base() {
+    // TODO: send signal
+    engine_thread_.join();
+  }
+ private:
+  std::thread engine_thread1_;
+  seastar::app_template app_;
+};
