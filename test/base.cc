40ca1c3dbe63b8f28802b552ccea3fb471a13c03
jasonyuchen <jasonyuchen@foxmail.com>
2021-09-25 10:24:23 +0800

    support gtest in seastar-based application

--- test/base.cc
+++ test/base.cc
@@ -2,7 +2,44 @@
 // Created by jason on 2021/9/15.
 //
 
-#include "gtest/gtest.h"
+#include "base.hh"
+
+#include <seastar/core/coroutine.hh>
+
+#include "util/signal.hh"
+
+using namespace std;
+using namespace seastar;
+
+void rafter_test_base::SetUpTestSuite() {
+  app_template::config app_cfg;
+  app_cfg.auto_handle_sigint_sigterm = false;
+  _app = std::make_unique<app_template>(std::move(app_cfg));
+  std::promise<void> pr;
+  auto fut = pr.get_future();
+  _engine_thread = std::thread([pr = std::move(pr)]() mutable {
+    return _app->run(
+        argc, const_cast<char **>(argv),
+        // We cannot use `pr = std::move(pr)` here as it will forbid compilation
+        // see https://taylorconor.com/blog/noncopyable-lambdas/
+        [&pr]() mutable -> seastar::future<> {
+          rafter::util::stop_signal stop_signal;
+          pr.set_value();
+          co_return co_await stop_signal.wait();
+        });
+  });
+  fut.wait();
+}
+
+void rafter_test_base::TearDownTestSuite() {
+  l.info("shutting down reactor engine with SIGTERM...");
+  auto ret = ::pthread_kill(_engine_thread.native_handle(), SIGTERM);
+  if (ret) {
+    l.error("send SIGTERM failed: {}", ret);
+    std::abort();
+  }
+  _engine_thread.join();
+}
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
--- test/base.hh
+++ test/base.hh
@@ -5,37 +5,73 @@
 #pragma once
 
 #include <thread>
+#include <future>
 
 #include <gtest/gtest.h>
+#include <seastar/core/alien.hh>
 #include <seastar/core/app-template.hh>
+#include <seastar/core/coroutine.hh>
 #include <seastar/core/future.hh>
 #include <seastar/core/reactor.hh>
-#include <seastar/core/coroutine.hh>
 
+#define RAFTER_TEST_(test_suite_name, test_name, parent_class, parent_id)      \
+  static_assert(sizeof(GTEST_STRINGIFY_(test_suite_name)) > 1,                 \
+                "test_suite_name must not be empty");                          \
+  static_assert(sizeof(GTEST_STRINGIFY_(test_name)) > 1,                       \
+                "test_name must not be empty");                                \
+  class GTEST_TEST_CLASS_NAME_(test_suite_name, test_name)                     \
+      : public parent_class {                                                  \
+   public:                                                                     \
+    GTEST_TEST_CLASS_NAME_(test_suite_name, test_name)() = default;            \
+    ~GTEST_TEST_CLASS_NAME_(test_suite_name, test_name)() override = default;  \
+    GTEST_DISALLOW_COPY_AND_ASSIGN_(GTEST_TEST_CLASS_NAME_(test_suite_name,    \
+                                                           test_name));        \
+    GTEST_DISALLOW_MOVE_AND_ASSIGN_(GTEST_TEST_CLASS_NAME_(test_suite_name,    \
+                                                           test_name));        \
+                                                                               \
+   private:                                                                    \
+    void TestBody() override;                                                  \
+    seastar::future<> SeastarBody();                                           \
+    static ::testing::TestInfo* const test_info_ GTEST_ATTRIBUTE_UNUSED_;      \
+  };                                                                           \
+                                                                               \
+  ::testing::TestInfo* const GTEST_TEST_CLASS_NAME_(test_suite_name,           \
+                                                    test_name)::test_info_ =   \
+      ::testing::internal::MakeAndRegisterTestInfo(                            \
+          #test_suite_name, #test_name, nullptr, nullptr,                      \
+          ::testing::internal::CodeLocation(__FILE__, __LINE__), (parent_id),  \
+          ::testing::internal::SuiteApiResolver<                               \
+              parent_class>::GetSetUpCaseOrSuite(__FILE__, __LINE__),          \
+          ::testing::internal::SuiteApiResolver<                               \
+              parent_class>::GetTearDownCaseOrSuite(__FILE__, __LINE__),       \
+          new ::testing::internal::TestFactoryImpl<GTEST_TEST_CLASS_NAME_(     \
+              test_suite_name, test_name)>);                                   \
+  void GTEST_TEST_CLASS_NAME_(test_suite_name, test_name)::TestBody() {        \
+    auto fut = seastar::alien::submit_to(                                      \
+        *seastar::alien::internal::default_instance,                           \
+        0,                                                                     \
+        [this] { return this->SeastarBody(); });                               \
+    fut.wait();                                                                \
+  }                                                                            \
+  seastar::future<>                                                            \
+  GTEST_TEST_CLASS_NAME_(test_suite_name, test_name)::SeastarBody()
 
-#define RAFTER_TEST_CASE_F(suite, name) \
-  TEST_F(suite, name) {                 \
-    auto f =                             \
-  }                                     \
-  seastar::future<> GTEST_TEST_CLASS_NAME_(suite, name)::SeastarTestBody()
+#define RAFTER_TEST_F(test_fixture, test_name)                                 \
+  RAFTER_TEST_(test_fixture, test_name, test_fixture,                          \
+               ::testing::internal::GetTypeId<test_fixture>())
 
-class rafter_test_base {
+class rafter_test_base : public ::testing::Test {
  public:
+  // dummy args, use actual argc, argv instead
   inline static const int argc = 3;
-  inline static const char* argv[] = {"rafter_test_base", ""};
-  rafter_test_base() {
-    engine_thread_ = std::thread([this] {
-      return app_.run(argc, const_cast<char **>(argv), []() -> seastar::future<> {
-        // TODO: handle signal
-        co_return;
-      });
-    });
-  }
-  ~rafter_test_base() {
-    // TODO: send signal
-    engine_thread_.join();
-  }
- private:
-  std::thread engine_thread1_;
-  seastar::app_template app_;
+  inline static const char *argv[] = {"rafter_test_base", "-c2", "-m1G"};
+
+  static void SetUpTestSuite();
+
+  static void TearDownTestSuite();
+
+ protected:
+  inline static std::thread _engine_thread;
+  inline static std::unique_ptr<seastar::app_template> _app;
+  inline static seastar::logger l = seastar::logger("rafter_test");
 };
