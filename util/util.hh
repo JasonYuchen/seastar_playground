27c902b04999c442efc43d13b55590958078bd81
jasonyuchen <jasonyuchen@foxmail.com>
2021-11-19 13:16:46 +0800

    minor update

--- util/util.hh
+++ util/util.hh
@@ -5,15 +5,17 @@
 #pragma once
 
 #include <functional>
+#include <type_traits>
 #include <utility>
 
 namespace rafter::util {
 
 struct pair_hasher {
   template<typename T>
-  std::size_t operator()(T&& pair) const {
+  std::size_t operator()(T&& pair) const noexcept {
     auto&& [one, two] = pair;
-    return std::hash<decltype(one)>{}(one) ^ std::hash<decltype(two)>{}(two);
+    return std::hash<std::remove_cvref_t<decltype(one)>>()(one) ^
+           std::hash<std::remove_cvref_t<decltype(two)>>()(two);
   }
 };
 
