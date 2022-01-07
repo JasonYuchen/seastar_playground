251b6951a5197cc3caa45b8f4960cee65bd2a4e3
jasonyuchen <jasonyuchen@foxmail.com>
2022-01-07 14:38:50 +0800

    raft peer tracking

--- core/remote.cc
+++ core/remote.cc
@@ -4,8 +4,122 @@
 
 #include "remote.hh"
 
+#include "util/error.hh"
+
 namespace rafter::core {
 
+bool remote::is_paused() const noexcept {
+  return state == wait || state == snapshot;
+}
 
+void remote::become_retry() noexcept {
+  if (state == snapshot) {
+    next = std::max(match, snapshot_index) + 1;
+  } else {
+    next = match + 1;
+  }
+  snapshot_index = 0;
+  state = retry;
+}
+
+void remote::become_wait() noexcept {
+  become_retry();
+  retry_to_wait();
+}
+
+void remote::become_replicate() noexcept {
+  next = match + 1;
+  snapshot_index = 0;
+  state = replicate;
+}
+
+void remote::become_snapshot(uint64_t index) noexcept {
+  snapshot_index = index;
+  state = snapshot;
+}
+
+void remote::retry_to_wait() noexcept {
+  if (state == retry) {
+    state = wait;
+  }
+}
+
+void remote::wait_to_retry() noexcept {
+  if (state == wait) {
+    state = retry;
+  }
+}
+
+void remote::replicate_to_retry() noexcept {
+  if (state == replicate) {
+    become_retry();
+  }
+}
+
+void remote::clear_pending_snapshot() noexcept {
+  snapshot_index = 0;
+}
+
+bool remote::try_update(uint64_t index) noexcept {
+  if (next < index + 1) {
+    next = index + 1;
+  }
+  if (match < index) {
+    wait_to_retry();
+    match = index;
+    return true;
+  }
+  return false;
+}
+
+void remote::optimistic_update(uint64_t last_index) {
+  if (state == replicate) {
+    next = last_index + 1;
+  } else if (state == retry) {
+    // do not optimistically update the `next` since the remote is in Retry
+    // state due to inflight snapshot or unreachable network
+    retry_to_wait();
+  } else {
+    throw util::failed_precondition_error("unexpected optimistic_update");
+  }
+}
+
+bool remote::try_decrease(
+    uint64_t rejected_index, uint64_t peer_last_index) noexcept {
+  if (state == replicate) {
+    if (rejected_index <= match) {
+      // The rejection must be stale if the progress has matched and
+      // `rejected_index` is smaller than `match`
+      return false;
+    }
+    // The `next` is set to `match` + 1 which is more conservative than the
+    // `next` = `next` - 1 proposed in Raft thesis.
+    next = match + 1;
+    return true;
+  }
+  if (rejected_index != next - 1) {
+    // The rejection must be stale if `rejected_index` does not match `next` - 1
+    // because the log_index of the replication_resp is set to `next` - 1 in
+    // `make_replicate_message`.
+    return false;
+  }
+  wait_to_retry();
+  next = std::max(uint64_t{1}, std::min(rejected_index, peer_last_index + 1));
+  return true;
+}
+
+void remote::responded_to() noexcept {
+  if (state == retry) {
+    become_replicate();
+  } else if (state == snapshot && match >= snapshot_index) {
+    become_retry();
+  }
+}
+
+std::string remote::debug_string() const {
+  return fmt::format(
+      "remote[match:{}, next:{}, state:{}, snapshot_index:{}, active:{}]",
+      match, next, uint8_t{state}, snapshot_index, active);
+}
 
 }  // namespace rafter::core
--- core/remote.hh
+++ core/remote.hh
@@ -4,10 +4,101 @@
 
 #pragma once
 
+#include <stdint.h>
+#include <string>
+
 namespace rafter::core {
 
 class remote {
  public:
+  enum state : uint8_t {
+    // Retry means leader could probe the remote after:
+    // 1. The remote has been Wait and now leader receives a heartbeat response
+    //    which means leader could probe the remote then, set remote as Retry.
+    // 2. The remote has been Wait or Replicate and now leader receives a
+    //    rejection of the replication, try to decrease the next index for the
+    //    remote and set the remote as Retry.
+    // 3. The remote has been Wait and now leader receives an acceptance of the
+    //    replication, try to increase the next & match index for the remote
+    //    and set the remote as Retry.
+    // 4. The leader receives an acceptance of replication, if the remote is in
+    //    Snapshot state and the match >= snapshotIndex, set the remote as Retry
+    //    and do probing.
+    // 5. The remote is unreachable, if it is Replicate then set it as Retry.
+    // Retry is an active state and the leader can send messages.
+    retry,
+    // Wait means leader should stop replicating after:
+    // 1. The remote has been Snapshot and leader receives the result, set the
+    //    remote as Wait whether the result is successful or failed.
+    //    If successful, just wait for the replication response which will be
+    //    sent once the remote is restored (the remote will responds a
+    //    replication response once restored the snapshot).
+    //    If failed, just wait for another heartbeat interval before next probe
+    //    (the ReportSnapshotStatus will be called when transport module failed
+    //    to send the snapshot which eventually calls becomeWait).
+    // 2. Leader usually optimistically updates the next for remotes, but if a
+    //    remote has been in Retry, set it as Wait to reduce redundant probes.
+    // Wait is an inactive state and the leader won't send messages.
+    wait,
+    // Replicate means the remote is eager for the replication after:
+    // 1. The remote has been Retry and leader receives an acceptance of the
+    //    replication, set the remote as Replicate.
+    // Replicate is an active state and the leader can send messages.
+    replicate,
+    // Snapshot means the remote is far behind the leader and needs a snapshot:
+    // 1. The remote is set as Snapshot if the required logs are compacted.
+    // Snapshot is an inactive state and the leader won't send messages.
+    snapshot,
+    num_of_state,
+  };
+
+  bool is_paused() const noexcept;
+
+  // The remote state is unknown, retry to find its index.
+  // See probe state in etcd:
+  // https://github.com/etcd-io/etcd/blob/master/raft/tracker/progress.go#L43
+  void become_retry() noexcept;
+
+  // The remote state is waiting due to inflight snapshot.
+  // The etcd does not have this state (retry + wait here = probe in etcd).
+  void become_wait() noexcept;
+
+  // The remote state is clear, keep replicating logs.
+  // See replicate state in etcd:
+  // https://github.com/etcd-io/etcd/blob/master/raft/tracker/progress.go#L43
+  void become_replicate() noexcept;
+
+  // The snapshot is inflight, stop replicating logs.
+  // See snapshot state in etcd:
+  // https://github.com/etcd-io/etcd/blob/master/raft/tracker/progress.go#L43
+  void become_snapshot(uint64_t index) noexcept;
+
+  void retry_to_wait() noexcept;
+  void wait_to_retry() noexcept;
+  void replicate_to_retry() noexcept;
+  void clear_pending_snapshot() noexcept;
+
+  // Will be called when receives replicate_resp, try to update the log track of
+  // the remote node, return true if updated.
+  bool try_update(uint64_t index) noexcept;
+
+  // Will be called when sends replicate messages, optimistically update the
+  // "next" for a remote in replicate state.
+  void optimistic_update(uint64_t last_index);
+
+  // Will be called when receives replication_resp with rejection, try to
+  // decrease the log track of the remote node, return true if decreased.
+  bool try_decrease(uint64_t rejected_index, uint64_t peer_last_index) noexcept;
+
+  void responded_to() noexcept;
+
+  std::string debug_string() const;
+
+  uint8_t match = 0;
+  uint8_t next = 0;
+  uint8_t snapshot_index = 0;
+  bool active = false;
+  enum state state = state::retry;
 };
 
 }  // namespace rafter::core
