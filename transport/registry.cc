9aa60ae49f4389a486799f0005ad7e1d02051f9f
jasonyuchen <jasonyuchen@foxmail.com>
2022-01-09 11:15:41 +0800

    transport: introduce node registry
    Signed-off-by: jasonyuchen <jasonyuchen@foxmail.com>

--- transport/registry.cc
+++ transport/registry.cc
@@ -6,6 +6,47 @@
 
 namespace rafter::transport {
 
+bool operator==(const peer_address& x, const peer_address& y) noexcept {
+  return x.address == y.address && x.port == y.port;
+}
 
+bool operator<(const peer_address& x, const peer_address& y) noexcept {
+  auto xsv = peer_address::to_string_view(x.address);
+  auto ysv = peer_address::to_string_view(y.address);
+  return (xsv < ysv) || (xsv == ysv && x.port < y.port);
+}
+
+std::string_view peer_address::to_string_view(
+    const seastar::net::inet_address& address) {
+  return {reinterpret_cast<const char*>(address.data()), address.size()};
+}
+
+std::ostream& operator<<(std::ostream& os, const peer_address& x) {
+  return os << x.address << ':' << x.port << ':' << x.cpu_id;
+}
+
+std::optional<peer_address> registry::resolve(protocol::group_id gid) {
+  auto it = _peer.find(gid);
+  if (it == _peer.end()) {
+    return std::optional<peer_address>{};
+  }
+  return it->second;
+}
+
+void registry::update(protocol::group_id gid, peer_address address) {
+  _peer.insert({gid, address});
+}
+
+void registry::remove(protocol::group_id gid) { _peer.erase(gid); }
+
+void registry::remove_cluster(uint64_t cluster_id) {
+  for (auto it = _peer.begin(); it != _peer.end();) {
+    if (it->first.cluster == cluster_id) {
+      it = _peer.erase(it);
+    } else {
+      ++it;
+    }
+  }
+}
 
 }  // namespace rafter::transport
--- transport/registry.hh
+++ transport/registry.hh
@@ -4,10 +4,51 @@
 
 #pragma once
 
+#include <seastar/core/sharded.hh>
+#include <seastar/net/inet_address.hh>
+#include <unordered_map>
+
+#include "protocol/raft.hh"
+#include "util/util.hh"
+
 namespace rafter::transport {
 
-class registry {
+struct peer_address {
+  seastar::net::inet_address address;
+  uint16_t port;
+  uint32_t cpu_id;
+  friend bool operator==(const peer_address& x, const peer_address& y) noexcept;
+  friend bool operator<(const peer_address& x, const peer_address& y) noexcept;
+  friend std::ostream& operator<<(std::ostream& os, const peer_address& x);
+  struct hash {
+    size_t operator()(const peer_address& id) const noexcept {
+      return std::hash<std::string_view>()(to_string_view(id.address));
+    }
+  };
+  peer_address(seastar::net::inet_address ip, uint16_t port, uint32_t cpu)
+    : address(ip), port(port), cpu_id(cpu) {}
+
+  static std::string_view to_string_view(
+      const seastar::net::inet_address& address);
+};
+
+class registry
+  : public seastar::async_sharded_service<registry>
+  , public seastar::peering_sharded_service<registry> {
  public:
+  // unengaged return means not found
+  std::optional<peer_address> resolve(protocol::group_id gid);
+
+  void update(protocol::group_id gid, peer_address address);
+
+  void remove(protocol::group_id gid);
+
+  void remove_cluster(uint64_t cluster_id);
+
+  seastar::future<> stop() { return seastar::make_ready_future<>(); }
+
+ private:
+  std::unordered_map<protocol::group_id, peer_address, util::pair_hasher> _peer;
 };
 
 }  // namespace rafter::transport
