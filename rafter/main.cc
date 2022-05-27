215078869065caf15e2be05d694a7cec3ae3ff07
jasonyuchen <jasonyuchen@foxmail.com>
2022-05-27 23:25:17 +0800

    rafter: main
    Signed-off-by: jasonyuchen <jasonyuchen@foxmail.com>

---
+++ rafter/main.cc
@@ -0,0 +1,78 @@
+//
+// Created by jason on 2022/5/22.
+//
+
+#include <seastar/core/app-template.hh>
+
+#include "rafter/logger.hh"
+#include "rafter/nodehost.hh"
+#include "rsm/session_manager.hh"
+#include "rsm/snapshotter.hh"
+#include "server/environment.hh"
+#include "storage/segment_manager.hh"
+#include "transport/exchanger.hh"
+#include "transport/registry.hh"
+#include "util/signal.hh"
+
+using namespace seastar;
+
+static int rafter_main(int argc, char** argv, char** env) {
+  app_template::config app_cfg;
+  app_cfg.name = "rafter";
+  app_cfg.description = "FIXME";
+  app_cfg.auto_handle_sigint_sigterm = false;
+  app_template app{std::move(app_cfg)};
+  // TODO(jyc): program options here
+
+  sharded<rafter::storage::segment_manager> logdb;
+  sharded<rafter::transport::registry> registry;
+  sharded<rafter::transport::exchanger> rpc;
+  sharded<rafter::nodehost> nodehost;
+
+  return app.run(argc, argv, [&]() -> future<int> {
+    rafter::l.info("rafter initializing...");
+    auto&& opts = app.configuration();
+    rafter::util::stop_signal stop_signal;
+    // TODO(jyc): construct nodehost config via yaml
+    rafter::config config;
+    rafter::config::initialize(config);
+    co_await rafter::config::broadcast();
+
+    // TODO(jyc): starting procedure (draft)
+    //  1. constructs all sharded instances
+    //  2. start all rafter services except for messaging service
+    //  3. start messaging service, make sure all other services are up when
+    //     accepting peer's messages
+    //  4. reload all existing clusters (be careful that a message belonging to
+    //     cluster A may arrive before the initialization of cluster A)
+    //  5. start web service and accept normal requests
+
+    using rafter::server::environment;
+    auto partitioner = environment::get_partition_func();
+    auto snapshot_dir = environment::get_snapshot_dir_func(config.data_dir);
+    co_await logdb.start(std::move(partitioner));
+    co_await registry.start();
+    co_await rpc.start(std::ref(registry), std::move(snapshot_dir));
+    co_await nodehost.start(
+        std::move(config), std::ref(logdb), std::ref(registry), std::ref(rpc));
+    co_await logdb.invoke_on_all(&rafter::storage::segment_manager::start);
+    co_await registry.invoke_on_all(&rafter::transport::registry::start);
+    co_await rpc.invoke_on_all(&rafter::transport::exchanger::start);
+    // TODO(jyc): list and reload existing clusters in nodehost::start
+    co_await nodehost.invoke_on_all(&rafter::nodehost::start);
+
+    // TODO(jyc): tiny web service for http request/response and metrics
+
+    auto signum = co_await stop_signal.wait();
+    rafter::l.info("rafter exiting... with {}:{}", signum, ::strsignal(signum));
+    // TODO(jyc): stop and close all existing clusters in nodehost::stop
+    co_await nodehost.stop();
+    co_await rpc.stop();
+    co_await registry.stop();
+    co_await logdb.stop();
+  });
+}
+
+int main(int argc, char** argv, char** env) {
+  return rafter_main(argc, argv, env);
+}
